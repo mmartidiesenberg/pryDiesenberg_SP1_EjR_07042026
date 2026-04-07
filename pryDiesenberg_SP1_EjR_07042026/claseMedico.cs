aba79cd8314174ee3ce4f8576cb3f4cdd922a742
mmartidiesenberg <mmmartidiesenberg@gmail.com>
2026-04-07 09:36:19 -0300

    .

---
+++ pryDiesenberg_SP1_EjR_07042026/claseMedico.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryDiesenberg_SP1_EjR_07042026
+{
+    internal class claseMedico
+    {
+        public int Matricula { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Especialidad { get; set; }
+        public claseMedico(int matricula, string nombre, string apellido, int especialidad)
+        {
+            Matricula = matricula;
+            Nombre = nombre;
+            Apellido = apellido;
+            Especialidad = especialidad;
+        }
+
+        public void RegistrarMedicoBD()
+        {
+
+        }
+    }
+}
